53cc0d1b4c8efb007df62565bfa976f455a045f6
TheRedBrain <thetrueredbrain@gmail.com>
2025-03-28 15:06:29 +0100

    added compatibility data pack for 'Dungeons and Taverns' by NovaWostra

--- src/main/java/com/github/theredbrain/rpgenchanting/RPGEnchanting.java
+++ src/main/java/com/github/theredbrain/rpgenchanting/RPGEnchanting.java
@@ -80,6 +80,7 @@ public class RPGEnchanting implements ModInitializer {
 		ScreenHandlerTypesRegistry.init();
 		Optional<ModContainer> modContainer = FabricLoader.getInstance().getModContainer(MOD_ID);
 		if (modContainer.isPresent()) {
+			ResourceManagerHelper.registerBuiltinResourcePack(identifier("rpgenchanting_dungeons_and_taverns"), modContainer.get(), Text.translatable("resourcepack.rpgenchanting.rpgenchanting_dungeons_and_taverns.name"), ResourcePackActivationType.NORMAL);
 			ResourceManagerHelper.registerBuiltinResourcePack(identifier("rpgenchanting_more_rpg_series"), modContainer.get(), Text.translatable("resourcepack.rpgenchanting.rpgenchanting_more_rpg_series.name"), ResourcePackActivationType.NORMAL);
 			ResourceManagerHelper.registerBuiltinResourcePack(identifier("rpgenchanting_more_rpg_series_resources"), modContainer.get(), Text.translatable("resourcepack.rpgenchanting.rpgenchanting_more_rpg_series_resources.name"), ResourcePackActivationType.NORMAL);
 			ResourceManagerHelper.registerBuiltinResourcePack(identifier("rpgenchanting_rpg_series"), modContainer.get(), Text.translatable("resourcepack.rpgenchanting.rpgenchanting_rpg_series.name"), ResourcePackActivationType.NORMAL);
