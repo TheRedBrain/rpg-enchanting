6b38a9e8c5b2da1739aad3b75ca9fe93b6249fb9
TheRedBrain <thetrueredbrain@gmail.com>
2026-07-13 13:46:45 +0200

    new name overwrites

--- src/main/java/com/github/theredbrain/rpgenchanting/item/EnchantedItemNameHelper.java
+++ src/main/java/com/github/theredbrain/rpgenchanting/item/EnchantedItemNameHelper.java
@@ -44,49 +44,68 @@ public class EnchantedItemNameHelper {
 		}
 
 		Component text = itemStack.getCustomName();
-//		MutableComponent textPrefixOverwrite = Component.empty();
-//		MutableComponent textSuffixOverwrite = Component.empty();
+		MutableComponent textPrefixOverwrite = Component.empty();
+		MutableComponent textSuffixOverwrite = Component.empty();
+		String translationKeyPrefixOverwrite = "";
+		boolean usePrefixOverwrite = false;
+		boolean useSuffixOverwrite = false;
 		if (text == null) {
 
 			// checks if the combination of item and enchantments has a dedicated translation
 			// this is disabled when the item has a custom name
 			String translationKeyOverwrite = prefixEnchantmentTranslationKey + "." + prefixEnchantmentLevel + "." + itemStack.getItem().getDescriptionId() + "." + suffixEnchantmentTranslationKey + "." + suffixEnchantmentLevel;
 			MutableComponent textOverwrite = Component.translatable(translationKeyOverwrite);
-			RPGEnchanting.info("textOverwrite.getString(): " + textOverwrite.getString());
-			RPGEnchanting.info("translationKeyOverwrite: " + translationKeyOverwrite);
+//			RPGEnchanting.info("textOverwrite.getString(): " + textOverwrite.getString());
+//			RPGEnchanting.info("translationKeyOverwrite: " + translationKeyOverwrite);
 			if (!Objects.equals(textOverwrite.getString(), translationKeyOverwrite)) {
 				return textOverwrite;
 			}
 
-//			// prefix overwrite
-//			String translationKeyPrefixOverwrite = prefixEnchantmentTranslationKey + "." + prefixEnchantmentLevel + "." + itemStack.getItem().getDescriptionId();
-//			textPrefixOverwrite = Component.translatable(translationKeyPrefixOverwrite);
-//			RPGEnchanting.info("textPrefixOverwrite.getString(): " + textPrefixOverwrite.getString());
-//			RPGEnchanting.info("translationKeyPrefixOverwrite: " + translationKeyPrefixOverwrite);
-//			if (Objects.equals(textPrefixOverwrite.getString(), translationKeyPrefixOverwrite)) {
-//				translationKeyPrefixOverwrite = "";
-//				textPrefixOverwrite = Component.empty();
-//			}
-//
-//			// suffix overwrite
-//			String translationKeySuffixOverwrite = itemStack.getItem().getDescriptionId() + "." + suffixEnchantmentTranslationKey + "." + suffixEnchantmentLevel;
-//			textSuffixOverwrite = Component.translatable(translationKeySuffixOverwrite);
+			// suffix overwrite
+			String translationKeySuffixOverwrite = itemStack.getItem().getDescriptionId() + "." + suffixEnchantmentTranslationKey + "." + suffixEnchantmentLevel;
+			textSuffixOverwrite = Component.translatable(translationKeySuffixOverwrite);
 //			RPGEnchanting.info("textSuffixOverwrite.getString(): " + textSuffixOverwrite.getString());
 //			RPGEnchanting.info("translationKeySuffixOverwrite: " + translationKeySuffixOverwrite);
-//			if (Objects.equals(textSuffixOverwrite.getString(), translationKeySuffixOverwrite)) {
-//				translationKeySuffixOverwrite = "";
-//				textSuffixOverwrite = Component.empty();
-//			}
+			if (Objects.equals(textSuffixOverwrite.getString(), translationKeySuffixOverwrite)) {
+				translationKeySuffixOverwrite = "";
+				textSuffixOverwrite = Component.empty();
+			} else {
+				useSuffixOverwrite = true;
+			}
+
+			if (!useSuffixOverwrite) {
+				// prefix overwrite
+				translationKeyPrefixOverwrite = prefixEnchantmentTranslationKey + "." + prefixEnchantmentLevel + "." + itemStack.getItem().getDescriptionId();
+				textPrefixOverwrite = Component.translatable(translationKeyPrefixOverwrite);
+//				RPGEnchanting.info("textPrefixOverwrite.getString(): " + textPrefixOverwrite.getString());
+//				RPGEnchanting.info("translationKeyPrefixOverwrite: " + translationKeyPrefixOverwrite);
+				if (Objects.equals(textPrefixOverwrite.getString(), translationKeyPrefixOverwrite)) {
+					translationKeyPrefixOverwrite = "";
+					textPrefixOverwrite = Component.empty();
+				} else {
+					usePrefixOverwrite = true;
+				}
+			}
 
 			text = itemStack.getItemName();
 		}
-		if (!suffixEnchantmentString.isEmpty()) {
-			text = Component.translatable(suffixEnchantmentString, text);
+		if (!useSuffixOverwrite) {
+			if (!suffixEnchantmentString.isEmpty()) {
+				text = Component.translatable(suffixEnchantmentString, text);
+			}
+		} else {
+			text = textSuffixOverwrite;
 		}
-		if (!prefixEnchantmentString.isEmpty()) {
-//				if () {
-//				}
-			text = Component.translatable(prefixEnchantmentString, text);
+
+		if (usePrefixOverwrite) {
+			text = textPrefixOverwrite;
+			if (!suffixEnchantmentString.isEmpty()) {
+				text = Component.translatable(suffixEnchantmentString, text);
+			}
+		} else {
+			if (!prefixEnchantmentString.isEmpty()) {
+				text = Component.translatable(prefixEnchantmentString, text);
+			}
 		}
 		return text;
 	}
